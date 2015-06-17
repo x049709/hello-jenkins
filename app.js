ae9c7e8cadf3fd4584bc5464505f78718c6b24b3
Ed Bayudan <ed.bayudan@yahoo.com>
2015-06-17 17:55:07 -0400

    Switch test to hello yom jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello yo jenkins');
+  res.send('hello yom jenkins');
 });
 
 app.listen(process.env.PORT || 5000);
