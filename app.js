eee7d4e74e72effad499fd1c2bc2c4f007b64ff3
Ed Bayudan <ed.bayudan@yahoo.com>
2015-06-17 17:30:41 -0400

    Switch test to hello boy jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello eddie jenkins');
+  res.send('hello boy jenkins');
 });
 
 app.listen(process.env.PORT || 5000);
