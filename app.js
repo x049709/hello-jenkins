bb42cd7366376749f0a7cd357872066dfefac823
Ed Bayudan <ed.bayudan@yahoo.com>
2015-06-17 16:20:52 -0400

    Switch to hello eddie boy jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello ed jenkins');
+  res.send('hello eddie boy jenkins');
 });
 
 app.listen(process.env.PORT || 5000);
