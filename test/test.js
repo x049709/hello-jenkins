d2989db383e3d68ad0590b92b33a6c42f35359e1
Ed Bayudan <ed.bayudan@yahoo.com>
2015-06-17 16:57:07 -0400

    Switch test to hello eddie jenkins

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello eddie manlier jenkins', done);
+    request(app).get('/').expect('hello eddie  jenkins', done);
   });
 });
