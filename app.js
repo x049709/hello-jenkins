f84f4d0cd5e1cab414aa2728d70479e3410d9bda
Ed Bayudan <ed.bayudan@yahoo.com>
2015-06-17 18:36:32 -0400

    Switch test to hello yomm

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello yom');
+  res.send('hello yomm');
 });
 
 app.listen(process.env.PORT || 5000);
