1e5fefb0ec64ef832994490dc7ca0109ecc16385
Ed Bayudan <ed.bayudan@yahoo.com>
2015-06-18 19:33:27 -0400

    Add deploy yowel

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello yowe');
+  res.send('hello yowel');
 });
 
 app.listen(process.env.PORT || 5000);
